4baa5e19de5b6e061f8a4a58f8365e08686c0e38
KCsenge <csengekormoczy@gmail.com>
2022-10-04 11:48:05 +0200

    Enable Routing

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,14 +1,13 @@
-sap.ui.define([
-	"sap/ui/core/UIComponent"
-], function (UIComponent) {
-	"use strict";
+sap.ui.define(["sap/ui/core/UIComponent"], function (UIComponent) {
+  "use strict";
 
-	return UIComponent.extend("sap.ui.demo.nav.Component", {
-
-		metadata: {
-			manifest: "json"
-		}
-
-	});
-
+  return UIComponent.extend("sap.ui.demo.nav.Component", {
+    metadata: {
+      manifest: "json",
+    },
+    init: function () {
+      UIComponent.prototype.init.apply(this, arguments);
+      this.getRouter().initialize();
+    },
+  });
 });
